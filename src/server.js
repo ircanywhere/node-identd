d7778cc6142dab771960fe96cba5003f7c895e98
n0valyfe <rickihastings22@gmail.com>
2013-01-16 22:43:14 +0000

    Added a basic file structure and a LICENSE doc

---
+++ src/server.js
@@ -0,0 +1,10 @@
+/*
+ * node-ident
+ * 
+ * An ident daemon in node with a MongoDB and JSON backend
+ *
+ * Copyright (c) 2013 IRCAnywhere <support@ircanywhere.com>.
+ * Rights to this code are as documented in LICENSE.
+ */
+
+const server = require('net');
