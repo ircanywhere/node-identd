a63241a9ed120214e51700ad3e220e69f7fb271f
n0valyfe <rickihastings22@gmail.com>
2013-02-19 19:03:52 +0000

    Created the inter process event emitter

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@
 
 const util = require('util'),
 	  net = require('net'),
+	  ipem = require('ipevents'),
 	  respond = require('./respond');
 // include our modules
 
@@ -18,7 +19,9 @@ const util = require('util'),
  *
  * A server object containing everything
  */
-var Server = {};
+var Server = {
+	records: []
+};
 
 /*
  * Server::start
@@ -46,8 +49,53 @@ Server.start = function()
 	{
 		util.log('Now listening on ' + _this.ip + ':' + _this.port);
 		// ready to go
+
+		
 	});
 	// tell the socket server to listen on our port and ip
+};
+
+/*
+ * Server::createIpem
+ *
+ * A function which creates the inter process event emitter
+ */
+Server.createIpem = function()
+{
+	/*
+	 * An inter process event emitter which handles the RPC functions
+	 * basically two functions
+	 *
+	 * register - local port, remote port, uid
+	 * remove	- uid
+	 */
+	ipem = null;
+	ipem
+		.options({
+			restart: true,
+			delayRestart: 0,
+			useSocket: true,
+			socket: {
+				onlyConnect: false,
+				socketPath: null,
+				port: 7300,
+				host: 'localhost',
+				reconnect: true,
+				delayReconnect: 1000
+			}
+		})
+		.on('register', function(localPort, remotePort, uid)
+		{
+			console.log('registering:', localPort, remotePort, uid);
+		})
+		.on('remove', function(uid)
+		{
+			console.log('destroying:', localPort, remotePort, uid);
+		})
+		.on('error', function(error)
+		{
+
+		});
 };
 
 Server.start();
